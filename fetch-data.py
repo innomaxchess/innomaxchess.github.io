efbfc7c79d13cb0afdb7ef73371e0389f4681295
VictorXypCh <victorxyp.ch@gmail.com>
2023-11-18 15:46:27 +0700

    update nov-18 2023

---
+++ fetch-data.py
@@ -0,0 +1,53 @@
+import requests 
+from bs4 import BeautifulSoup
+import json
+from datetime import date
+
+#
+url = 'https://swissonlinetournament.com/Tournament/Rating/d6a0b23b18f346948836e3abe55804e6'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    table = soup.find('table')
+
+    if table:
+
+        json_data = []
+
+        for row in table.find_all('tr')[1:]:
+            row_data = {}
+
+            columns = row.find_all(['td','th'])
+
+            for idx, column in enumerate(columns):
+                if idx == 1:
+                    row_data['name'] = column.text.strip()
+                elif idx == 2:
+                    row_data['total'] = column.text.strip()
+                else:
+                    row_data[column.text.strip()] = column.text.strip()
+
+
+            json_data.append(row_data)
+
+        json_string = json.dumps(json_data, indent=2)
+
+        today = date.today()
+        file_path = 'month/'+today.strftime('%Y-%m-%d') + '.json'
+
+        with open(file_path, 'w') as json_file:
+            json.dump(json_string, json_file, indent=2)
+
+        print(json_string)
+        print(f"JSON data has been written to {file_path}")
+    else:
+        print("Table not found on the page")
+else: 
+
+    print("Failed to retrieve the page.")
+
